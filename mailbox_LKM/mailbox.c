b0b2d375e941483674e091b66101a2f391766b12
Pat L <ppatlong@gmail.com>
2014-03-03 10:09:02 -0500

    Don't create mailbox for kernel processes

--- mailbox_LKM/mailbox.c
+++ mailbox_LKM/mailbox.c
@@ -28,26 +28,28 @@ void mailbox_exit() {
 }
 
 Mailbox* mailbox_create(pid_t owner) {
-    Mailbox* mailbox;
+    Mailbox* mailbox = NULL;
 
-    printk(KERN_INFO "Creating new mailbox for %d", owner);
+    if(current->mm) {
+        printk(KERN_INFO "Creating new mailbox for %d", owner);
 
-    // Allocate it
-    mailbox = kmem_cache_alloc(mailbox_cache, 0);
+        // Allocate it
+        mailbox = kmem_cache_alloc(mailbox_cache, 0);
 
-    // Initialize info
-    mailbox->owner = owner;
-    mailbox->message_count = 0;
-    mailbox->message_max = 32; // Magic constant
-    mailbox->stopped = 0;
-    atomic_set(&mailbox->waiting, 0);
+        // Initialize info
+        mailbox->owner = owner;
+        mailbox->message_count = 0;
+        mailbox->message_max = 32; // Magic constant
+        mailbox->stopped = 0;
+        atomic_set(&mailbox->waiting, 0);
 
-    // Initialize lists
-    INIT_LIST_HEAD(&mailbox->messages);
-    INIT_HLIST_NODE(&mailbox->hash_table_entry);
+        // Initialize lists
+        INIT_LIST_HEAD(&mailbox->messages);
+        INIT_HLIST_NODE(&mailbox->hash_table_entry);
 
-    // Initialize the wait queue
-    init_waitqueue_head(&mailbox->modify_queue);
+        // Initialize the wait queue
+        init_waitqueue_head(&mailbox->modify_queue);
+    }
 
     return mailbox;
 }
